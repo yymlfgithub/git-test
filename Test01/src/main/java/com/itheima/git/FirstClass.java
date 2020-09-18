dbde7cada516bd90b750791de7b71967410c7026
xiaoli <yymlf996539@163.com>
2020-09-18 09:38:29 +0800

    把本地工程添加到git4

---
+++ Test01/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class FirstClass {
+
+    public static void main(){
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+
+
+}
